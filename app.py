0017a3abca9364a5e8094eee2438608bbdb5c00d
7zrv <4014133@gmail.com>
2024-06-12 16:50:46 +0900

    기초 세팅

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
+# app.py
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from model import User, Book
+from db import db
 
-app = Flask(__name__)
-app.config.from_object('config.Config')
 
-db = SQLAlchemy(app)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
 
-with app.app_context():
-    db.create_all()
+    db.init_app(app)
 
-@app.route("/")
-def home():
-    return render_template('index.html')
+    from apis import user_update
+    app.register_blueprint(user_update.user_bp1)
+
+    from views import main_views
+    app.register_blueprint(main_views.bp)
+
+    @app.route("/")
+    def home():
+        return render_template('index.html')
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
