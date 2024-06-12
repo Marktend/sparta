265680b76c07871e5f54718e20ebb9e408dffb03
7zrv <4014133@gmail.com>
2024-06-12 14:14:35 +0900

    기초 세팅

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
-from flask import Flask
-import config
-from db_connect import db
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
-def create_app(test_config=None):
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    # Config 설정
-    if test_config is None:
-        app.config.from_object(config)
-    else:
-        app.config.update(test_config)
+# AWS RDS 연결 정보
+aws_db = {
+    "user": "admin",
+    "password": "123456789a",
+    "host": "database-1.civgmymwfbly.ap-northeast-2.rds.amazonaws.com",
+    "port": "3306",
+    "database": "spartan_18",
+}
 
-    db.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{aws_db['user']}:{aws_db['password']}@{aws_db['host']}:{aws_db['port']}/{aws_db['database']}?charset=utf8mb4"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    return app
+db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route("/")
+def home():
+
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
-    create_app().run()
+    app.run(debug=True)
